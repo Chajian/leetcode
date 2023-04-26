6efaf8e27ff26dddfbe17495dc4e36589d4bec3a
trusted <936796603@qq.com>
2023-04-26 17:34:45 +0800

    answers the 104 question of leetcode way: iteration

--- main/test/interview/binarytree/a104.java
+++ main/test/interview/binarytree/a104.java
@@ -1,12 +1,33 @@
 package interview.binarytree;
 
 
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
- * way: recursiveness
+ * way: iteration
  */
 public class a104 {
     public int maxDepth(TreeNode root) {
-        if(root==null)return 0;
-        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+        int level = 0;
+        Queue<TreeNode> queue = new LinkedList();
+        if(root!=null)
+            queue.add(root);
+        while(queue.size()>0){
+            int num = queue.size();
+            while(num-->0){
+                TreeNode treeNode = queue.poll();
+                if(treeNode.left!=null)
+                    queue.add(treeNode.left);
+                if(treeNode.right!=null)
+                    queue.add(treeNode.right);
+            }
+            level++;
+        }
+        return level;
     }
 }
