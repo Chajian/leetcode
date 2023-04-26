d765001abc082d6b7135877881439e42356b4435
trusted <936796603@qq.com>
2023-04-26 19:11:39 +0800

    answers the 100 question of leetcode way: iteration

--- main/test/interview/binarytree/a100.java
+++ main/test/interview/binarytree/a100.java
@@ -1,11 +1,37 @@
 package interview.binarytree;
 
+import com.sun.source.tree.Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * way: iteration
+ */
 public class a100 {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null&&q==null)return true;
-        if(p==null||q==null)return false;
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(p);
+        Queue<TreeNode> queue2 = new LinkedList();
+        queue2.add(q);
+        while(queue.size()>0){
+            if(queue.size()!=queue2.size())return false;
+            int num = queue.size();
+            while(num-->0){
+                TreeNode pp = queue.poll();
+                TreeNode qq = queue2.poll();
+                if(pp==null&&qq==null)
+                    continue;
+                if(pp==null||qq==null)return false;
 
-        if(p.val != q.val) return false;
-        return true&&isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+                if(pp.val!=qq.val)return false;
+                queue.add(pp.left);
+                queue.add(pp.right);
+
+                queue2.add(qq.left);
+                queue2.add(qq.right);
+            }
+        }
+        return true;
     }
 }
