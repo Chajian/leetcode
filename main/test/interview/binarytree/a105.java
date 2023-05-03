40b77c69ff5cd02ef07d17cc9337dd9b9ce5f06b
trusted <936796603@qq.com>
2023-05-03 16:16:09 +0800

    question: 105 way: Stack,iterator

--- main/test/interview/binarytree/a105.java
+++ main/test/interview/binarytree/a105.java
@@ -3,37 +3,39 @@ package interview.binarytree;
 import entity.TreeNode;
 import org.junit.Test;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * way: recursion
+ */
 public class a105 {
 
-    int on = 0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return buildTree(preorder,inorder,0,preorder.length-1,on);
-    }
-
-    public TreeNode buildTree(int[] preorder, int[] inorder,int left,int right,int target) {
-        if(left>=preorder.length||right>=preorder.length||left>right) {
-            on--;
-            return null;
-        }
-        else if(left==right)return new TreeNode(preorder[target]);
-        else{
-            TreeNode treeNode = new TreeNode(preorder[target]);
-            int index = left;
-            for(int i = left ; i <= right;i++){
-                if(inorder[i]==preorder[target]) {
-                    index = i;
-                    break;
-                }
+        Deque<TreeNode> deque = new ArrayDeque();
+        TreeNode root = new TreeNode(preorder[0]);
+        deque.push(root);
+        for(int i = 1 ,j=0; i < preorder.length ;i++){
+            TreeNode node = new TreeNode(preorder[i]);
+            if(deque.size()<1||inorder[j]!=deque.peek().val){
+                deque.peek().left = node;
             }
-            treeNode.left=buildTree(preorder,inorder,left,index-1,++on);
-            treeNode.right=buildTree(preorder,inorder,index+1,right,++on);
-            return treeNode;
+            else{
+                TreeNode treeNode = null;
+                while(deque.size()>0&&deque.peek().val==inorder[j]){
+                    treeNode = deque.pop();
+                    j++;
+                }
+                treeNode.right = node;
+            }
+            deque.push(node);
         }
+        return root;
     }
 
     @Test
     public void test(){
-        TreeNode treeNode = buildTree(new int[]{3,2,1,4},new int[]{1,2,3,4});
+        TreeNode treeNode = buildTree(new int[]{3,9,20,15,7},new int[]{9,3,15,20,7});
         System.out.println();
     }
 }
