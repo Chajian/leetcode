638cfe2b0b732d6edec3d07bb35db3392a1538ed
trusted <936796603@qq.com>
2023-05-05 20:04:57 +0800

    question:114 way:queue,DFS rate:0.24

--- main/test/interview/binarytree/a114.java
+++ main/test/interview/binarytree/a114.java
@@ -1,36 +1,46 @@
 package interview.binarytree;
 
+import com.sun.source.tree.Tree;
 import entity.TreeNode;
 import org.junit.Test;
 import tools.Binary;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class a114 {
 
-    TreeNode cur;
+
+    Queue<TreeNode> queue = new LinkedList();
     public void flatten(TreeNode root) {
         if(root==null)return;
-        TreeNode temp = root.right;
-        if(root.left!=null) {
-            root.right = root.left;
-            cur = root.left;
-            flatten(root.left);
+
+        addQueue(root);
+        TreeNode pre = null;
+        while(queue.size()>0){
+            TreeNode cur = queue.poll();
+            if(pre!=null) {
+                pre.right = cur;
+                pre.left = null;
+            }
+            pre = cur;
         }
-        root.left = null;
-        if(cur==null)cur = root;
-        if(temp!=null) {
-            cur.right = temp;
-            cur = temp;
-            flatten(temp);
-        }
+    }
+
+    public void addQueue(TreeNode root) {
+        queue.add(root);
+        if(root.left!=null)
+            addQueue(root.left);
+        if(root.right!=null)
+            addQueue(root.right);
     }
 
     @Test
     public void test(){
         Binary binary = new Binary();
-        TreeNode root = binary.ganerateTreeByLevel(new Integer[]{1,null,2,null,null,3});
+        TreeNode root = binary.ganerateTreeByLevel(new Integer[]{1,2,5,3,4,null,6});
         flatten(root);
         System.out.println();
 
