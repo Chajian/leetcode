112751185d358d0d6539d5a749add5f65054e809
trusted <936796603@qq.com>
2023-05-14 19:09:41 +0800

    question:139 way:dp rate:0.99

--- main/test/interview/dp/a139.java
+++ main/test/interview/dp/a139.java
@@ -11,12 +11,20 @@ public class a139 {
         boolean[] dp = new boolean[s.length()+1];
         HashSet<String> set = new HashSet<>();
         int n = s.length();
+        int maxWorld = 0;
+        int minWorld = 999;
         for(String ss:wordDict){
             set.add(ss);
+            if(ss.length()>maxWorld)
+                maxWorld = ss.length();
+            if(ss.length()<minWorld)
+                minWorld = ss.length();
         }
+
+
         dp[0] = true;
         for(int i = 1; i <= n;i++){
-            for(int j = 0; j < i&&!dp[i];j++) {
+            for(int j = i<=maxWorld?0:i-maxWorld; j <= i-minWorld&&!dp[i];j++) {
                 String sub = s.substring(j, i);
                 if (set.contains(sub)) dp[i] = dp[j];
             }
