391408c211550f0266aefc295015daf91590694d
trusted <936796603@qq.com>
2023-05-19 16:30:04 +0800

    question:746 way:dp rate:0.1

--- main/test/interview/dp/a746.java
+++ main/test/interview/dp/a746.java
@@ -7,18 +7,14 @@ import java.util.Map;
 
 //liuyubobobo
 public class a746 {
-    Map<Integer,Integer> memory = new HashMap<>();
     public int minCostClimbingStairs(int[] cost) {
-        return move(cost.length,cost);
-    }
-
-    public int move(int n,int[]cost){
-        if(n==0||n==1)return 0;
-        if(memory.get(n)!=null)
-            return memory.get(n);
-        int result = Math.min((cost[n-1]+move(n-1,cost)),(cost[n-2]+move(n-2,cost)));
-        memory.put(n,result);
-        return result;
+        int[] dp = new int[cost.length+1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for(int i = 2 ; i <= cost.length ; i++){
+            dp[i] = Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+        }
+        return dp[cost.length];
     }
 
     @Test
