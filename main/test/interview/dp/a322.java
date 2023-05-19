6e48716354523f86c24f3cf3c237a25a73b0db67
trusted <936796603@qq.com>
2023-05-19 09:41:29 +0800

    question:322 way:dp rate:0.25

--- main/test/interview/dp/a322.java
+++ main/test/interview/dp/a322.java
@@ -1,4 +1,32 @@
 package interview.dp;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class a322 {
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        dp[0] = 0;
+        for(int i = 1 ; i <= amount ; i++){
+            int min = Integer.MAX_VALUE;
+            for(int j = 0; j < coins.length ;j++){
+                if(i-coins[j]>=0&&dp[i-coins[j]]<min) {
+                    min = dp[i-coins[j]]+1;
+                }
+                dp[i] = min;
+            }
+        }
+        return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
+    }
+
+
+    @Test
+    public void test(){
+        System.out.println(coinChange(new int[]{186,419,83,408},6249));
+    }
 }
