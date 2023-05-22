963b68bef2e3c9eaeeaecfca4f96164f177c9dad
trusted <936796603@qq.com>
2023-05-22 14:13:38 +0800

    question: 343 way:math rate:1

--- main/test/interview/dp/a343.java
+++ main/test/interview/dp/a343.java
@@ -1,20 +1,16 @@
 package interview.dp;
 
 public class a343 {
-    int[] dp;
     public int integerBreak(int n) {
-        dp = new int[n<6?6:n];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-        dp[4] = 6;
-        dp[5] = 9;
-
-
-        for(int i = 6 ; i < n ; i++){
-            dp[i] = 3*dp[i-3];
+        if(n<=3) return n-1;
+        int a = n/3;
+        int b = n%3;
+        switch (b){
+            case 0:
+                return (int) Math.pow(3,a);
+            case 1:
+                return 4*(int) Math.pow(3,a-1);
         }
-        return dp[n-1];
+        return 2*(int) Math.pow(3,a);
     }
 }
