bd0f25591319de0353a60da9db5d1604a6002288
trusted <936796603@qq.com>
2023-05-27 16:42:27 +0800

    question: 416 way: dp rate:0.44

--- main/test/interview/dp/a416.java
+++ main/test/interview/dp/a416.java
@@ -1,11 +1,13 @@
 package interview.dp;
 
+import com.sun.tools.javac.Main;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class a416 {
+    int[] dp;
     public boolean canPartition(int[] nums) {
         int target = 0;
         for(int i = 0 ; i < nums.length ;i++){
@@ -15,26 +17,16 @@ public class a416 {
             return false;
         else
             target/=2;
-        int[][]dp = new int[nums.length][target+1];
-        //init
-        for(int i = 0 ; i <= target ;i++){
-            if(i<nums[0]){
-                dp[0][i] =0;
-            }
-            else{
-                dp[0][i] = nums[0];
-            }
+        dp = new int[target+1];
+        for(int i = 0 ; i < nums.length ;i++){
+            for(int j = target ; j >= nums[i];j--)
+                dp[j] = Math.max(dp[j],dp[j-nums[i]]+nums[i]);
         }
-        for(int i = 1; i < nums.length ;i++){
-            for(int j = 1 ; j <= target&&dp[i][j]<=target;j++){
-                dp[i][j] = Math.max(dp[i-1][j],j-nums[i]>=0?dp[i-1][j-nums[i]]+nums[i]:0);
-            }
-        }
-        return dp[nums.length-1][target]==target;
+        return dp[target]==target;
     }
 
     @Test
     public void test(){
-        System.out.println(canPartition(new int[]{1,2,3,5}));
+        System.out.println(canPartition(new int[]{14,9,8,4,3,2}));
     }
 }
