163614f1b18c89798db11c2acea09634fad4eab9
trusted <936796603@qq.com>
2023-05-27 20:51:50 +0800

    question: 1049 way:dp rate:0.97

--- main/test/interview/dp/a1049.java
+++ main/test/interview/dp/a1049.java
@@ -5,38 +5,20 @@ import org.junit.Test;
 import java.util.*;
 
 public class a1049 {
-    int dp[][];
+    int dp[];
     public int lastStoneWeightII(int[] stones) {
         int sum = 0;
         for(int i = 0 ; i < stones.length ;i++){
             sum+=stones[i];
         }
-        dp = new int[stones.length][sum+1];
         int target =sum/2;
-            return Math.abs(sum-2*add(stones,0,0,target));
-    }
-
-    public int add(int[] stones,int i,int num,int target){
-        if(i>=stones.length||num==target)
-            return num;
-
-        int next = 0,source = 0;
-        if(dp[i][num+stones[i]]==0){
-            next = add(stones, i + 1, num + stones[i], target);
-            dp[i][num+stones[i]] = next;
+        dp = new int[target+1];
+        for(int i = 0 ; i < stones.length ;i++){
+            for(int j = target ; j>= stones[i] ;j--){
+                dp[j] = Math.max(dp[j],dp[j-stones[i]]+stones[i]);
+            }
         }
-        else
-            next = dp[i][num+stones[i]];
-        if(dp[i][num]==0){
-            source = add(stones, i + 1, num, target);
-            dp[i][num] = source;
-        }
-        else
-            source = dp[i][num];
-
-        if(Math.abs(target-next)<=Math.abs(target-source))
-            return next;
-        return source;
+        return sum-2*dp[target];
     }
 
     @Test
