3c99419139c605e78e00c0b1f910437efb1093f5
trusted <936796603@qq.com>
2023-11-21 22:34:12 +0800

    merge

--- main/test/interview/dp/again/a322.java
+++ main/test/interview/dp/again/a322.java
@@ -2,61 +2,8 @@ package interview.dp.again;
 
 import org.junit.Test;
 
-<<<<<<< HEAD
-public class a322 {
-
-    int res;
-    int[] memory;
-
-    public int coinChange(int[] coins, int amount) {
-        res = Integer.MAX_VALUE;
-        memory = new int[amount];
-        sortDes(coins,0,coins.length-1);
-        return dfs(coins,amount,0,0)==Integer.MAX_VALUE?-1:res;
-    }
-
-    public int dfs(int[] coins,int mount,int i,int time){
-        if(mount < 0||i>=coins.length||time>=res||mount<0)
-            return -1;
-        if(mount==0) {
-            res = Math.min(res,time);
-            return time;
-        }
-        if(memory[mount-1]!=0)
-            return memory[mount-1];
-        dfs(coins,mount-coins[i],i,time+1);
-        dfs(coins,mount-coins[i],i+1,time+1);
-        dfs(coins,mount,i+1,time);
-        memory[mount-1] = res;
-        return memory[mount-1];
-    }
-
-    public void sortDes(int[] num,int i,int j){
-        if(i>=j)
-            return;
-        int start = i,end = j,middle = num[i],middleI = start;
-        while(start<end){
-            while(start<end&&num[end]<middle)
-                end--;
-            num[start] = num[end];
-            while(start<end&&num[start]>=middle)
-                start++;
-            num[end] = num[start];
-        }
-        num[start] = middle;
-        sortDes(num,i,start-1);
-        sortDes(num,start+1,j);
-    }
-
-    @Test
-    public void test(){
-        System.out.println(coinChange(new int[]{411,412,413,414,415,416,417,418,419,420,421,422},9864));
-        System.out.println(coinChange(new int[]{1,2,5},11));
-        System.out.println(coinChange(new int[]{186,419,83,408},6249));
-    }
-
-=======
 import java.util.Arrays;
+
 
 public class a322 {
     public int coinChange(int[] coins, int amount) {
@@ -92,5 +39,4 @@ public class a322 {
         System.out.println(coinChange(new int[]{3,7,405,436},8839));
         System.out.println(coinChange(new int[]{411,412,413,414,415,416,417,418,419,420,421,422},9864));
     }
->>>>>>> 0b91ff3fef0828212502be256d1fa019f4954f21
 }
